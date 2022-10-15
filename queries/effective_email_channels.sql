82e271084d30865435f3be72ac51a68da7f66a93
morgan <47616998+smorgsweeks@users.noreply.github.com>
2022-10-15 11:17:55 -0400

    Update effective_email_channels.sql

--- queries/effective_email_channels.sql
+++ queries/effective_email_channels.sql
@@ -5,8 +5,8 @@ marketing_channel AS "Marketing Channel"
 , marketing_channel_category AS "Marketing Channel Category"
 , count(e.hashed_user_id) AS "Count of Emails Subscribed"
 , round(count(e.hashed_user_id)/count(*),4)*100 AS "% of Email Subscription Converting Sessions"
-FROM test_fact_sessions s
-LEFT JOIN test_emails e
+FROM fact_sessions s
+LEFT JOIN emails e
     ON e.hashed_user_id = s.hashed_user_id
     AND e.received_at BETWEEN s.session_started_at AND s.session_ended_at
 WHERE marketing_channel != 'direct'
