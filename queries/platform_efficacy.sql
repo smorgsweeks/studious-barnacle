ca25eab0bdf27e5ff3c397f5f0b65ae45c0f83d9
morgan <47616998+smorgsweeks@users.noreply.github.com>
2022-10-15 11:18:20 -0400

    Update platform_efficacy.sql

--- queries/platform_efficacy.sql
+++ queries/platform_efficacy.sql
@@ -3,7 +3,7 @@ SELECT
 marketing_channel
 , marketing_channel_category
 , COUNT(*)
-FROM test_fact_sessions
+FROM fact_sessions
 WHERE marketing_channel != 'direct'
 GROUP BY 1,2
 ORDER BY 3 DESC
@@ -16,7 +16,7 @@ marketing_channel
 , COUNT(distinct hashed_user_id) AS count_of_users
 , round(avg(session_duration_minutes),2) AS average_session_duration
 , round(avg(page_views),2) AS average_session_pageviews
-FROM test_fact_sessions
+FROM fact_sessions
 WHERE marketing_channel != 'direct' 
 AND is_bounced_session = FALSE -- want to remove bounced sessions for better insights into behaviors
 GROUP BY 1,2
@@ -30,7 +30,7 @@ marketing_channel
 , count(*) AS count_of_sessions
 , count_if(is_bounced_session = TRUE) AS count_of_bounced_sessions
 , round(count_if(is_bounced_session = TRUE) /count(*),2) AS bounce_rate
-FROM test_fact_sessions
+FROM fact_sessions
 WHERE marketing_channel != 'direct' 
 GROUP BY 1,2
 ORDER BY 3 DESC
